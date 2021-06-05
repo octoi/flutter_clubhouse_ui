6c2cb8c4178417421221a30d1d0f4b4e71302c6b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-05 16:34:38 +0530

    app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,31 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(CupertinoIcons.search, size: 28.0),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.envelope_open, size: 26.0),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.calendar, size: 28.0),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.bell, size: 28.0),
+          ),
+        ],
+      ),
+    );
   }
 }
