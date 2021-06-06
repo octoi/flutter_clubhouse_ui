d7d8a611f2c3e3058b4ea533e92c2b8f74d989bf
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-06 09:33:47 +0530

    styled card

--- lib/widgets/room_card.dart
+++ lib/widgets/room_card.dart
@@ -11,20 +11,28 @@ class RoomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Text(
-              '${room.club} 🏡'.toUpperCase(),
-              style: Theme.of(context)
-                  .textTheme
-                  .overline!
-                  .copyWith(fontWeight: FontWeight.w600, letterSpacing: 1.0),
-              overflow: TextOverflow.ellipsis,
-            ),
-          ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '${room.club} 🏡'.toUpperCase(),
+                style: Theme.of(context).textTheme.overline!.copyWith(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1.0,
+                    ),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
         ),
       ),
     );
