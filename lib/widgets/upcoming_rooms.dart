9465f4f5704d0a6b2e04b98845eefa112bcc2a05
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-06 09:06:26 +0530

    upcoming rooms widget

---
+++ lib/widgets/upcoming_rooms.dart
@@ -0,0 +1,16 @@
+import 'package:clubhouse_ui/data.dart';
+import 'package:flutter/material.dart';
+
+class UpcomingRooms extends StatelessWidget {
+  final List<Room> upcomingRooms;
+
+  UpcomingRooms({
+    Key? key,
+    required this.upcomingRooms,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
