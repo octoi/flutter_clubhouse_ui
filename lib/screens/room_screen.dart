1f0d4c5fd498d7a61f514fbcc1bfbce750cb18b5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-06 10:23:43 +0530

    all rooms leading

--- lib/screens/room_screen.dart
+++ lib/screens/room_screen.dart
@@ -1,4 +1,5 @@
 import 'package:clubhouse_ui/data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RoomScreen extends StatelessWidget {
@@ -8,6 +9,17 @@ class RoomScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leadingWidth: 120.0,
+        leading: TextButton.icon(
+          style: TextButton.styleFrom(primary: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(CupertinoIcons.chevron_down),
+          label: const Text('All Rooms'),
+        ),
+      ),
+    );
   }
 }
