a7365998843958661eaccadb34661a6345207759
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-06 10:13:25 +0530

    menu button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -93,6 +93,34 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
+          Positioned(
+            bottom: 60.0,
+            right: 40.0,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    CupertinoIcons.circle_grid_3x3_fill,
+                    size: 28.0,
+                  ),
+                ),
+                Positioned(
+                  right: 4.6,
+                  bottom: 11.8,
+                  child: Container(
+                    height: 16.0,
+                    width: 16.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Theme.of(context).accentColor,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
