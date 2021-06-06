7e4a184710059e1db9bf8518952b7bc68f88e2be
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-06 09:29:20 +0530

    accepted room

--- lib/widgets/room_card.dart
+++ lib/widgets/room_card.dart
@@ -1,6 +1,14 @@
+import 'package:clubhouse_ui/data.dart';
 import 'package:flutter/material.dart';
 
 class RoomCard extends StatelessWidget {
+  final Room room;
+
+  RoomCard({
+    Key? key,
+    required this.room,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Card();
